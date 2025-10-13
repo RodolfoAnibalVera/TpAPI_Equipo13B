29bf430d05e14c3c586d6d6913b42341f623fd7e
RodolfoAnibalVera <vrodolfoanibal@gmail.com>
2025-10-12 22:25:24 -0300

    Agrego logica para agregar Articulo, creo e implemento ArticuloDto

--- api-CatalogoProducto/Controllers/ArticuloController.cs
+++ api-CatalogoProducto/Controllers/ArticuloController.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using dominio;
+using negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using dominio;
-using negocio;
+using System.Web.UI.WebControls.WebParts;
+using api_CatalogoProducto.Models;
 
 namespace api_CatalogoProducto.Controllers
 {
@@ -29,8 +31,19 @@ namespace api_CatalogoProducto.Controllers
         }
 
         // POST: api/Articulo
-        public void Post([FromBody]string value)
+        public void Post([FromBody]ArticuloDto art)
         {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            Articulo nuevo = new Articulo();
+            nuevo.Codigo = art.Codigo;
+            nuevo.Nombre = art.Nombre;
+            nuevo.Descripcion = art.Descripcion;
+            nuevo.Imagenes = art.Imagenes;
+            nuevo.Marca = new Marca { Id = art.IdMarca };
+            nuevo.Categoria = new Categoria { Id = art.IdCategoria };
+            nuevo.Precio = art.Precio;
+
+            negocio.agregar(nuevo);
         }
 
         // PUT: api/Articulo/5
---
+++ api-CatalogoProducto/Models/ArticuloDto.cs
@@ -0,0 +1,26 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace api_CatalogoProducto.Models
+{
+    public class ArticuloDto
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        public string Imagenes { get; set; }
+
+        public int IdMarca { get; set; }
+        public int IdCategoria { get; set; }
+        public decimal Precio
+        {
+            get; set;
+
+        }
+    }
+}
